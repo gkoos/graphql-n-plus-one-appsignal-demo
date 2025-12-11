ef365e6d66d21d75c6b4ddb95a28fe9be38a909c
Gabor Koos <gabor.koos@gmail.com>
2025-12-11 23:10:49 +0000

    Cosmetic tweaks

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,24 +1,20 @@
 export const resolvers = {
   Query: {
     users: async (_, __, { db }) => {
-      console.log('📍 Query: Fetching all users');
       return db.user.findMany();
     },
     posts: async (_, __, { db }) => {
-      console.log('📍 Query: Fetching all posts');
       return db.post.findMany();
     }
   },
   Post: {
     author: async (post, _, { db }) => {
       // This is the N+1 problem: called once per post
-      console.log(`🔄 Query: Fetching author for post ${post.id}`);
       return db.user.findUnique({ where: { id: post.authorId } });
     }
   },
   User: {
     posts: async (user, _, { db }) => {
-      console.log(`🔄 Query: Fetching posts for user ${user.id}`);
       return db.post.findMany({ where: { authorId: user.id } });
     }
   }
